b1391a0eef810a712eac2e48296f1dc7ede8cb41
Albert Albala <albert@mediatribe.net>
2023-07-04 14:39:04 -0400

    Please squash this commit in the pull request

--- src/traits/CommonUtilities.php
+++ src/traits/CommonUtilities.php
@@ -127,11 +127,7 @@ trait CommonUtilities {
    * Mockable wrapper around fgetcsv().
    */
   public function fgetcsv($handle) {
-    $return = fgetcsv($handle);
-    if (!$return) {
-      throw new \Exception('fgetcsv() failure for ' . $handle);
-    }
-    return $return;
+    return fgetcsv($handle);
   }
 
   /**
