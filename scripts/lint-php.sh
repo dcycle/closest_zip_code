53a01532b5a03b9e65732da37dbbf711ec480945
Albert Albala <albert@mediatribe.net>
2024-10-23 10:06:30 -0400

    Add Drupal 11 support and drop Drupal 9 support (#7)

--- scripts/lint-php.sh
+++ scripts/lint-php.sh
@@ -3,15 +3,25 @@
 # Lint php files.
 #
 
-echo 'Linting PHP files'
-echo 'If you are getting a false negative, use:'
+echo 'Linting PHP files with https://github.com/dcycle/docker-php-lint'
+echo 'If you are getting a false positive, use:'
 echo ''
 echo '// @codingStandardsIgnoreStart'
 echo '...'
 echo '// @codingStandardsIgnoreEnd'
 echo ''
+echo 'To automatically fix errors, you can run:'
+echo ''
+echo './scripts/lint-php-fix.sh'
+echo ''
 
-docker run --rm -v "$(pwd)"/src:/code dcycle/php-lint:2 \
-  --standard=DrupalPractice /code
-docker run --rm -v "$(pwd)"/src:/code dcycle/php-lint:2 \
-  --standard=Drupal /code
+docker run --rm \
+  -v "$(pwd)"/src:/code \
+  dcycle/php-lint:3 \
+  --standard=DrupalPractice \
+  /code
+docker run --rm \
+  -v "$(pwd)"/src:/code \
+  dcycle/php-lint:3 \
+  --standard=Drupal \
+  /code
